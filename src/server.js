297b4bc7ba9f92423b4b81800de8a7889658a2d6
MatheusMorata <matlucar09@hotmail.com>
2024-05-28 12:37:24 -0300

    feat: server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
-require('dotenv').config({ path: '../.env' })
+require('dotenv').config({path: '../.env'})
 const express = require('express')
 const server = express() 
 const PORT = process.env.PORT
+const BASE_ROUTE = process.env.BASE_ROUTE
+
+server.use(express.json())
+
+// Rotas
+server.use(BASE_ROUTE, rotasTarefas)
 
 server.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
