e33cf84898fa3f55b214e84e4a7fa1fa6b86c3b8
MatheusMorata <matlucar09@hotmail.com>
2024-05-28 13:58:43 -0300

    feat: tarefasModel.js

---
+++ src/model/tarefasModel.js
@@ -0,0 +1,33 @@
+const {DataTypes, Model} = require('sequelize')
+const conexao = require('../database/conexao')
+
+class Tarefas extends Model{}
+
+Tarefas.init({
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    titulo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    descricao: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    dataCriacao: {
+      type: DataTypes.DATE,
+      allowNull: false
+    }
+}, {
+    conexao,
+    modelName: 'Tarefas'
+})
