16aba7cfc7211602348b2e9eb011c444e36c7fcc
MatheusMorata <matlucar09@hotmail.com>
2024-05-28 13:33:18 -0300

    feat: tarefas.js

--- src/router/tarefas.js
+++ src/router/tarefas.js
@@ -1,11 +1,40 @@
 const express = require('express')
-const router = express()
+const router = express.Router()
 
+// Rota para cadastrar uma tarefa
 router.post('/cadastrar', async (req, res) => {
     try {
-      res.status(200).json({message: 'Deu bom'});
+      res.status(200).json({message: 'Deu bom'})
     } catch (error) {
-      res.status(500).json({ message: 'Deu ruim'});
+      res.status(500).json({ message: 'Deu ruim'})
     }
 });
-  
+
+// Rota para visualizar todas as tarefas 
+router.get('/visualizar', async (req, res) => {
+  try {
+    res.status(200).json({message: 'Deu bom'})
+  } catch (error) {
+    res.status(500).json({ message: 'Deu ruim'})
+  }
+});
+
+// Rota para alterar uma tarefa, por id
+router.put('/alterar/:id', async (req, res) => {
+  try {
+    res.status(200).json({message: 'Deu bom'})
+  } catch (error) {
+    res.status(500).json({ message: 'Deu ruim'})
+  }
+});
+
+// Rota para deletar uma tarefa, por id
+router.delete('/deletar/:id', async (req, res) => {
+  try {
+    res.status(200).json({message: 'Deu bom'})
+  } catch (error) {
+    res.status(500).json({ message: 'Deu ruim'})
+  }
+});
+
+module.exports = router
