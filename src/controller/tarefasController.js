1ac30e29c4bdefeb377a59fde64b7bc410534896
MatheusMorata <matlucar09@hotmail.com>
2024-05-28 17:21:28 -0300

    feat: tarefasController.js

---
+++ src/controller/tarefasController.js
@@ -0,0 +1,54 @@
+const model = require('./model/tarefas')
+
+class tarefasController{
+
+    async cadastrar(tarefa){
+        try{
+            await model.create(tarefa)
+        }catch(error){
+            throw new Error(error)
+        }
+    }
+
+    async visualizar(){
+        try{
+            const visualizar = await model.findAll()
+            return visualizar
+        }catch(error){
+            throw new Error(error)
+        }
+    }
+
+    async alterar(idTarefa, novaTarefa){
+        try {
+            const [linhas,[tarefaAtualizada]] = await Funcionario.update(novaTarefa, {
+                where: {
+                    id: idTarefa
+                },
+                returning: true 
+            });
+            if (linhas === 0) {
+                console.log("Nenhuma tarefa encontrada com o ID fornecido.")
+            } else {
+                console.log("Tarefa atualizada com sucesso")
+            }
+        } catch (error) {
+            console.error("Erro ao atualizar a tarefa", error)
+            throw error; 
+        }
+    }
+
+    async deletar(idTarefa){
+        try{
+            const funcionario = await Funcionario.destroy({
+                where: {
+                  id: funcId
+                }
+            })
+        }catch(error){
+            throw new Error(error)
+        }
+    }
+}
+
+module.exports = tarefasController
