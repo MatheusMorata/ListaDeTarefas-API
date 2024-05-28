4e18484f62e3e36456bd431a6dbdb51977e80f3b
MatheusMorata <matlucar09@hotmail.com>
2024-05-28 19:11:07 -0300

    feat: rotas.js

--- src/router/rotas.js
+++ src/router/rotas.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const controller = require('../controller/tarefasController')
+const objeto = new controller() 
 
 // Rota para cadastrar uma tarefa
 router.post('/cadastrar', async (req, res) => {
     try {
+      await objeto.cadastrar(req.body)
       res.status(200).json({message: 'Deu bom'})
     } catch (error) {
       res.status(500).json({ message: 'Deu ruim'})
@@ -13,7 +16,8 @@ router.post('/cadastrar', async (req, res) => {
 // Rota para visualizar todas as tarefas 
 router.get('/visualizar', async (req, res) => {
   try {
-    res.status(200).json({message: 'Deu bom'})
+    const resultado = await objeto.visualizar()
+    res.status(200).json(resultado)
   } catch (error) {
     res.status(500).json({ message: 'Deu ruim'})
   }
@@ -22,6 +26,9 @@ router.get('/visualizar', async (req, res) => {
 // Rota para alterar uma tarefa, por id
 router.put('/alterar/:id', async (req, res) => {
   try {
+    const idTarefa = req.params.id 
+    const novaTarefa = req.body
+    await objeto.alterar(idTarefa,novaTarefa)
     res.status(200).json({message: 'Deu bom'})
   } catch (error) {
     res.status(500).json({ message: 'Deu ruim'})
@@ -31,6 +38,8 @@ router.put('/alterar/:id', async (req, res) => {
 // Rota para deletar uma tarefa, por id
 router.delete('/deletar/:id', async (req, res) => {
   try {
+    const idTarefa = req.params
+    await objeto.deletar(idTarefa)
     res.status(200).json({message: 'Deu bom'})
   } catch (error) {
     res.status(500).json({ message: 'Deu ruim'})
