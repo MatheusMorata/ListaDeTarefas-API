ff309c4038d852ae38e8162d18f857a8f8428f47
MatheusMorata <matlucar09@hotmail.com>
2024-05-28 14:09:56 -0300

    feat: server.js

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config({path: '../.env'})
 const express = require('express')
 const server = express() 
 const rotasTarefas = require('./router/rotas')
+const conexao = require('./database/conexao')
 const PORT = process.env.PORT
 const BASE_ROUTE = process.env.BASE_ROUTE
 
@@ -14,3 +15,13 @@ server.use(BASE_ROUTE, rotasTarefas)
 server.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+// Conexão com o banco de dados
+(async () => {
+    try {
+      await conexao.sync();
+    } catch (error) {
+      console.error('Erro ao sincronizar tabelas:', error);
+    }
+})();
+  
