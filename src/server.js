8fdab0e9a0ef7bf984c09ab58ac8f7b574d53d27
MatheusMorata <matlucar09@hotmail.com>
2024-05-30 10:09:42 -0300

    refactor: server.js

--- src/server.js
+++ src/server.js
@@ -4,18 +4,14 @@ const server = express()
 const rotasTarefas = require('./router/rotas')
 const conexao = require('./database/conexao')
 const swaggerUi = require('swagger-ui-express');
-const swaggerFile = require('./swagger-output.json');
-const bodyParser = require('body-parser');
 const PORT = process.env.PORT
 const BASE_ROUTE = process.env.BASE_ROUTE
 
 server.use(express.json())
-server.use(bodyParser.json())
 
 
 // Rotas
 server.use(BASE_ROUTE, rotasTarefas)
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 server.listen(PORT, () => {
   console.log(`Servidor online  http://localhost:${PORT}`)
